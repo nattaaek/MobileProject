148b8ed005e311c06e52780bc85562ba31e1cb11
Alzack Connell <alzackconnell@Alzacks-Mac.local>
2017-10-15 14:14:47 +0700

    first

---
+++ HarvestFarm/HarvestFarm/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HarvestFarm
+//
+//  Created by Alzack Connell on 10/15/2560 BE.
+//  Copyright © 2560 Alzack Connell. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
